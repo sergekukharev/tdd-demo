1119ae94362c61fcfa80d356c0084c88513645a4
Sergei Kukhariev <serge.kukharev@gmail.com>
2019-10-09 16:50:38 -0300

    Made sure unit tests are running.

---
+++ katas/DummyTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Sk\TddDemo;
+
+use PHPUnit\Framework\TestCase;
+
+final class DummyTest extends TestCase
+{
+	public function testPhpunitWorks(): void
+	{
+		self::assertTrue(true);
+	}
+}
