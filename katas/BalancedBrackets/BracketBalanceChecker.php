0979c6cc31e9c4e63ad4d989b8476d9f0dfb5a63
Sergei Kukhariev <serge.kukharev@gmail.com>
2019-10-09 18:56:42 -0300

    Stack solution.

--- katas/BalancedBrackets/BracketBalanceChecker.php
+++ katas/BalancedBrackets/BracketBalanceChecker.php
@@ -6,25 +6,60 @@ namespace Sk\TddDemo\BalancedBrackets;
 
 final class BracketBalanceChecker
 {
+    private $stack;
+
+    public function __construct()
+    {
+        $this->stack = new \SplStack();
+    }
+
     public function isValid(string $input): bool
     {
-        $counter = 0;
         $length = strlen($input);
 
         for ($i = 0; $i < $length; $i++) {
-            if ($input[$i] === '[') {
-                $counter++;
-            }
+            $current = $input[$i];
 
-            if ($input[$i] === ']') {
-                $counter--;
-            }
-
-            if ($counter < 0) {
+            if ($this->isClosingBracket($current) && $this->stack->isEmpty()) {
                 return false;
             }
+
+            if ($this->isOpeningBracket($current)) {
+                $this->stack->push($current);
+            }
+
+            if (!$this->isClosingBracket($current)) {
+                continue;
+            }
+
+            if ($this->isCorrespondingBracket($this->stack->top(), $current)) {
+                $this->stack->pop();
+                continue;
+            }
+
+            $this->stack->push($current);
         }
 
-        return $counter === 0;
+        return $this->stack->isEmpty();
+    }
+
+    private function isOpeningBracket(string $i): bool
+    {
+        return $i === '[';
+    }
+
+    private function isClosingBracket($current): bool
+    {
+        return $current === ']';
+    }
+
+    private function isCorrespondingBracket($opening, $closing): bool
+    {
+        if ($closing === ']' && $opening === '[') {
+            return true;
+        }
+        return false;
     }
 }
+
+
